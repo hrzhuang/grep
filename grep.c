5b39a500adfbe6c64210899ffb0150b3b8cb7ca7
Ricky Zhuang <hr.zhuang@outlook.com>
2021-04-10 11:05:36 -0400

    Add error messages

--- grep.c
+++ grep.c
@@ -1,15 +1,25 @@
+#include <stdio.h>
+
 #include "re.h"
 #include "str.h"
 #include "str_util.h"
 
 int main(int argc, char **argv) {
-    if (argc < 2)
+    if (argc < 2) {
+        printf("%s: Missing regular expression.\n", argv[0]);
         return 2;
+    }
+    if (argc > 2) {
+        printf("%s: Too many arguments.\n", argv[0]);
+        return 2;
+    }
     struct str *re_str = str_from(argv[1]);
     struct re *re = re_compile(re_str);
     str_del(re_str);
-    if (!re)
+    if (!re) {
+        printf("%s: Invalid regular expression.\n", argv[0]);
         return 2;
+    }
     bool matched = false;
     for (struct str *line = get_line(); line; line = get_line()) {
         if (re_match(re, line)) {
